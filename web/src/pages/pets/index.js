1f418aa727bc4b873212b9044448e93d0bfa2e40
Kendsdavs <kendra.wieck@gmail.com>
2016-12-10 19:37:56 -0500

    added filter to search for pet names

--- web/src/pages/pets/index.js
+++ web/src/pages/pets/index.js
@@ -1,34 +1,49 @@
 const React = require('react')
 const {Link} = require('react-router')
 const data = require('../../utils/data')()
+const {filter} = require('ramda')
 
 const Pets = React.createClass({
   getInitialState() {
     return {
       pets: [],
+      filtered: [],
 
     }
   },
   componentDidMount() {
 
     data.list("pets")
-      .then(rows => {
-        console.log("these are the pets ", rows.docs)
-        this.setState({pets: rows.docs})
-      })
+      .then(pets => this.setState({filtered: pets.docs, pets: pets.docs})
+      )
+      // .then(pets => {
+      //   console.log("here are the pets", pets.docs)
+      //   return pets
+      // })
+  },
+  filter(e) {
+    this.setState({filtered:
+      filter(
+        pet => pet.name.indexOf(e.target.value) === 0,
+      this.state.pets
+      )
+    })
   },
   render() {
     const list = pet => <li key={pet._id}>
       <Link to={`/pets/${pet._id}/show`}>
-      {pet.name + " The " + pet.animal_type}
+      {pet.name}
     </Link>
 
       </li>
     return (
       <div>
         <h1>Pets</h1>
+          <input
+            onChange={this.filter}
+            placeholder="search" type="text"></input>
         <ul>
-          {this.state.pets.map(list)}
+          {this.state.filtered.map(list)}
         </ul>
         <Link to="/pets/new">Add Pet</Link>
         ||
