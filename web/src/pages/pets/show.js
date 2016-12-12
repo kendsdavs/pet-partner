986fedbae56c41e4376a3f090ac525fe40741865
Kendsdavs <kendra.wieck@gmail.com>
2016-12-12 12:54:46 -0500

    update pet show page

--- web/src/pages/pets/show.js
+++ web/src/pages/pets/show.js
@@ -7,25 +7,18 @@ const PetCard = require('./card')
 const Confirm = require('../../components/confirm')
 const PetPartnerNav = require('../../components/navbar')
 
-
 const Pet = React.createClass({
     getInitialState() {
-        return {
-            pet: {},
-            removed: false,
-            showconfirm: false,
-            procedures:[]}
+        return {pet: {}, removed: false, showconfirm: false, procedures: []}
     },
     componentDidMount() {
         data.get("pets", this.props.params.id).then(res => this.setState({pet: res}))
-        data.list('procedures')
-            .then(procedures => {
-                console.log("procedures are here ", procedures)
-                return procedures
-            })
-          //.then(procedures => procedures = procedures.docs)
-          .then(procedures => procedures.filter(proc => proc.parent_id === this.state.pet._id))
-          .then(procedures => this.setState({procedures: procedures}))
+        data.list('procedures').then(procedures => {
+            console.log("procedures are here ", procedures)
+            return procedures
+        })
+        //.then(procedures => procedures = procedures.docs)
+            .then(procedures => procedures.filter(proc => proc.parent_id === this.state.pet._id)).then(procedures => this.setState({procedures: procedures}))
     },
     handleCancel(e) {
         e.preventDefault()
@@ -43,8 +36,7 @@ const Pet = React.createClass({
     render() {
         return (
             <div>
-                <PetPartnerNav />
-                {this.state.removed
+                <PetPartnerNav/> {this.state.removed
                     ? <Redirect to="/pets"/>
                     : null}
                 {this.state.showconfirm
@@ -55,26 +47,28 @@ const Pet = React.createClass({
                     ? null
                     : <div>
                         <div className="container">
-                            <PetCard pet={this.state.pet} />
+                            <PetCard pet={this.state.pet}/>
                         </div>
+                        
                         <main>
 
-                        {this.state.pet._id ? <Procedures petID={this.state.pet._id} /> : null}
-                        <nav>
-                            <Link to="/pets">Back to Pets</Link>
-                            |
-                            <a href="#" onClick={this.handleRemove}>Remove Pet</a>
-                            |
-                            <Link to={`/pets/${this.state.pet._id}/edit`}>Edit Pet</Link>
-                        </nav>
-                    </main>
+                            {this.state.pet._id
+                                ? <Procedures petID={this.state.pet._id}/>
+                                : null}
+                            <nav>
+                                <Link to="/pets">Back to Pets</Link>
+                                |
+                                <a href="#" onClick={this.handleRemove}>Remove Pet</a>
+                                |
+                                <Link to={`/pets/${this.state.pet._id}/edit`}>Edit Pet</Link>
+                            </nav>
+                        </main>
                     </div>
-                }
+}
             </div>
 
-          )
+        )
 
-
-}
+    }
 })
 module.exports = Pet
