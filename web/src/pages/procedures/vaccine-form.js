4f58021f22c49e16aff157e923563565450e02c2
Kendsdavs <kendra.wieck@gmail.com>
2016-12-13 07:02:58 -0500

    made cards for vaccines. remove is wip

--- web/src/pages/procedures/vaccine-form.js
+++ web/src/pages/procedures/vaccine-form.js
@@ -1,44 +1,80 @@
 const React = require('react')
-const {FormGroup, Button, ControlLable, Grid, Row, Col, Form, Checkbox} = require('react-bootstrap')
+// const {FormGroup, Button, ControlLable, Grid, Row, Col, Form, Checkbox} = require('react-bootstrap')
 //const Vacbox = require("../../components/checkbox")
 
 const items =["vaccine 1", "vaccine 2", "vaccine 3"]
 
 let VaccineForm = React.createClass({
-  handleFormSubmit(e) {
-    e.preventDefault();
-    for (let checkbox of this.selectedCheckboxes) {
-      console.log(checkbox, 'is selected.')
+  getInitialState() {
+    return {
+    vaccines: [
+      {
+      name: "Lyme Disease",
+      _id: 1
+      },
+      {
+      name: "Distemper",
+      _id: 2
+    },{
+      name: "Adenovirus 2",
+      _id: 3
+    },{
+      name: "Parvovirus",
+      _id: 4
+    },{
+      name: "Leptospira",
+      _id: 5
+    }],
+
+    injections: [{
+      name: "Coronavirus",
+      _id: 6
+    },{
+      name: "Bordetella",
+      _id: 7
+    }]
+  }
+},
+  addVac(vaccine) {
+    return(e) => {
+      let injections = this.state.injections.filter(injection =>
+        injection._id !== vaccine._id)
+      this.setState({injections: [vaccine, ...injections]})
     }
   },
-  toggleCheckbox(label) {
-    if (this.selectedCheckboxes.has(label)) {
-      this.selectedCheckboxes.delete(label)
-    } else {
-      this.selectedCheckboxes.add(label)
+  removeInjection(i) {
+    return (e) => {
+      let injections = this.state.injections.filter(inject => inject._id !== i._id)
+      this.setState({injections})
     }
-  },
-  componentWillMount() {
-    console.log("new set created")
-    this.selectedCheckboxes = new Set()
-  },
-  createCheckbox(label) {
-    return <Checkbox
-      value={label}
-      handleCheckboxChange={this.toggleCheckbox}
-      key={label} />
-  },
-  createCheckboxes() {
-    return items.map(this.createCheckbox)
   },
   render() {
     return (
-      <Grid><Row><Col xs={12}>
-      <Form onSubmit={this.handleFormSubmit}>
-        {this.createCheckbox()}
-      <Button type="submit">Save Vaccines</Button>
-    </Form>
-    </Col></Row></Grid>
+      <div>
+      <div>
+        <h2>Add Vaccine to Medical History</h2>
+         {this.state.vaccines.map(v =>
+           <article className="mw5 dib bg-white br3 pa3 pa4-ns ma3 ba b--black-10">
+            <div className="tc">
+              <img src="http://tachyons.io/img/avatar_1.jpg" className="br-100 h4 w4 dib ba b--black-05 pa2" title="Kitty staring at you" />
+              <h1 className="f3 mb2">{v.name + v._id}</h1>
+              <button onClick={this.addVac(v)}>Add</button>
+            </div>
+          </article>
+         )}
+      </div>
+      <div>
+        <h3>Vaccine History</h3>
+        <ul>
+          {this.state.injections.map(i =>
+            <li key={i._id}>
+              {i.name}
+              <button onClick={this.removeInjection}>Remove</button>
+            </li>
+          )}
+        </ul>
+      </div>
+      </div>
     )
   }
 })
